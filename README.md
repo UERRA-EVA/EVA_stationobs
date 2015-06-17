c6ad38ae6dd22b37390918dff0f5109aa9a949e2
Borsche Michael <mborsche@oflws227.dwd.de>
2015-06-17 16:58:47 +0200

    working on README file
    - adding further explanations into README and Setrings.R

--- README.md
+++ README.md
@@ -2,17 +2,43 @@ EVA_stationobs
 ==============
 UERRA common evaluation procedure: assessing uncertainties in reanalysis by evaluation against station observations.
 
-Station observations are taken from the CDC-ftp server of DWD and cover Germany. A further data source may be added later.
+This package uses German station observations to compare against global (ERA20C and ERA-Interim) and regional (COSMO HErZ) reanalyses by plotting and analysing station and co-located reanalysis time series. The verification so far is performed on monthly means. Station observations are downloaded by the package during run-time from the CDC-ftp server of DWD.
+
+The reanalysis data had been download from ECMWF (global reanalyses) and directly from Uni Bonn (regional reanalysis) and post processed to a time series of monthly means. These data files are necessary for the package to run properly and can be downloaded from ECMWF's server ecgate at /scratch/ms/de/ded3/data/EVA_stationobs/.
+
+Further data sources may be added later, i.e., other regional reanalyses or station observation when or if available.
+
+So far, this package is distributed as a source package, i.e., the pure source code is provided.
+With the package "devtools" it can be installed; RStudio provides easy access to build functionality.
+
+Installation:
+-------------
+1. System requirements
+Since this package handles netCDFfiles the following libraries need to be installed:
+ * netcdf-bin
+ * libnetcdf-dev
+
+2. Necessary R dependencies
+ * netcdf
+ * netcdf.tools
+ * xts
+
+3. Nice-to-have R dependencies
+ * devtools
+
+4. Clone github repository
+ * git clone https://github.com/UERRA-EVA/EVA_stationobs.git
+
+Configuration of Settings.R
+---------------------------
+The *Settings.R* file holds the settings of variable configurations to the script.
+Please refer to the explanations within this file for further details.
+
 
 Running the package:
 --------------------
-Verifiaction is performed against monthly means of ERA20C and ERA-Interim global reanalyses as well as the COSMO HErZ regional reanalysis.
-These data files need to be downloaded prior to running this package. They can be accessed at ECMWF's server ecgate at /scratch/ms/de/ded3/data/EVA_stationobs/.
 
-So far, this package is distributed as a source package, i.e., the pure source code is provided.
-With tha package "devtools" it can be installed; RStudio provides easy access to build functionality.
-
-After installation, the file "./AnalyseWindspeed.R Settings.R" with its settings file Settings.R needs to be run.
+In order to run the program , the file "./AnalyseWindspeed.R" needs to be executedwith its settings file Settings.R needs to be run.
 In "Settings.R" data files and plotting options need to be set. The folders holding the data and the output plots need to be specified.
 
 
--- Settings.R
+++ Settings.R
@@ -1,41 +1,61 @@
 a4width = 29.7/2.54
 a4height = 21./2.54
 
-# this switch concerns the period to analyse and depends on the availability of the
-# reanalyses
+# this switch concerns the time period to analyse and depends on the availability
+# of the reanalyses:
+# * short.term: 2007-01 to 2010-12 (arbitrarily chosen short term analysis)
+# * mid.term:   1998-01 to 2010-12 (availability of COSMO HErZ reanalysis)
+# * long.term:  1979-01 to 2010-12 (availablity of ERA-Interim reanalysis to end of HErZ reanalysis)
+# * all.term:   1901-01 to 2010-12 and 2014-12 (availability of ERA20C reanalysis to end of global and regional reanalysis, respectively)
 time.switch = c("long.term")  # short.term, mid.term, long.term, all.term
-# this switch concerns the resolution of the ERA20C and ERAI reanalyses (HR is the
-# version interpolated to 0.125°)
+# this switch concerns the resolution of the ERA20C and ERA-Interim reanalyses
+# (HR is the version interpolated to 0.125°)
 res.switch = c("HighRes")   # HighRes, OrigRes
 
 #=== Paths ===
+# path to store resulting plots into
 outdir = "./output/"
 
 #=== ERA20C ===
-era20c.HRes.fname = paste0("./data/10m100m_WindSpeedDirection_ERA20C_19012010_MonMean.nc")
-era20c.ORes.fname = paste0("./data/10m100m_WindSpeedDirection_ERA20C_1900to2010_MonMean.nc")
+# filename to ERA20C reanalysis file
+era20c.HRes.fname = "./data/10m100m_WindSpeedDirection_ERA20C_19012010_MonMean.nc"
+era20c.ORes.fname = "./data/10m100m_WindSpeedDirection_ERA20C_1900to2010_MonMean.nc"
+# variable names to read from above files
 era20c.param = "windspeed_10m"
 era20c100.param = "windspeed_100m"
 
 #=== ERA-I ===
+# filename to ERA-Interim reanalysis file
 eraI.HRes.fname = "./data/ERAInterim_10mWindspeed_MonMean_highres.nc"
 eraI.ORes.fname = "./data/ERAInterim_10mWindspeed_MonMean.nc"
+# variable names to read from above files
 eraI.param = "si10"
 
 #=== HErZ ===
+# filename to COSMO HErZ reanalysis file
 herz.fname = "./data/WindSpeed_HErZ_MonMean_19982014.nc"
 herz.grid = "./data/COSMO_REA6_CONST_withOUTsponge.grb"
+# variable names to read from above files
 herz10.param = "windspeed_10m"
 herz116.param = "windspeed_116m"
 
 #=== Station data ===
-daily = TRUE # station data based on daily (T) or hourly (F) measurements
+# station data based on daily (T) or hourly (F) measurements
+daily = FALSE
+# filename of station name list
 station.daily.fname = "./data/KL_Tageswerte_Beschreibung_Stationen.txt"
-station.hourly.fname = "./data/FF_Stundenwerte_Beschreibung_Stationen.txt"
+station.hourly.fname = "./data/FF_Stundenwerte_Beschreibung_Stationen_wind.txt"
 
 #=== plotting ===
+# The following switches decided on which plots to generate
+
+# monthly mean time series between reanalyses and station data at 10m height
 plot.EraStatComp = T
+# monthly mean time series between ERA20C and COSMO HErZ reanalyses at 100m height
 plot.100mEraHerz = T
-plot.EraStationSeasons = T
+# only specific seasons of reanalyses and station data at 10m height - NOT YET FINISHED
+plot.EraStationSeasons = F
+# only specific months of reanalyses and station data at 10m height
 plot.EraStationMonths = T
+# PDF score between station data and each reanalysis at 10m height 
 plot.PDFscore = T
